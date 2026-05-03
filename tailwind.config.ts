2dac68ae14f676ca0196860e1062f0beac214e2e
taraqur <taraqur003@gmail.com>
2026-05-03 19:27:19 +0600

    build problem solved

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 import daisyui from "daisyui";
 
-const config: Config = {
+const config: any = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",

23fdfecdc6b6297141361ed8fbafe5a51c3b2176
taraqur <taraqur003@gmail.com>
2026-05-04 16:38:27 +0600

    problem solve

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,5 +2,6 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
     baseURL: "https://assignment-8-seven-alpha.vercel.app"
+});
 
 export const { signIn, signUp, useSession } = authClient;
